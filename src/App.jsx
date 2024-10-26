17dc9b8edde65bbe51224aa607cd57483ce25391
Huu Phuc Nguyen <nhphuc236@gmail.com>
2024-10-26 13:16:45 -0500

    Initial Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+    return (   
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+  )
+}
+
+export default App
